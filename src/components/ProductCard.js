591237595db973d82a7c62e64a2d5cfdd715eccb
Manasa <mail2manasak@gmail.com>
2024-06-23 10:01:24 +0530

    redux store configured

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../store";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <div className="border p-4 rounded shadow-sm">
       <img
@@ -10,7 +18,10 @@ const ProductCard = ({ product }) => {
       />
       <h2 className="text-xl font-bold mt-2">{product.name}</h2>
       <p className="mt-1">${product.price}</p>
-      <button className="bg-blue-600 text-white p-2 rounded mt-2">
+      <button
+        className="bg-blue-600 text-white p-2 rounded mt-2"
+        onClick={handleAddToCart}
+      >
         Add to Cart
       </button>
     </div>
---
+++ src/utils/cartSlice.js
@@ -0,0 +1,14 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
