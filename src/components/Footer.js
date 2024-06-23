c5ad656af069f715d61be4813a04a2181cb732c2
Manasa <mail2manasak@gmail.com>
2024-06-23 09:48:29 +0530

    footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white p-4 text-center">
+      <p>&copy; 2024 E-Commerce. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
